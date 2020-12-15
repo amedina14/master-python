f25d467d376971ff899cecc67719de1efd90fa96
Adrian Medina <medinalarry96@gmail.com>
2020-12-15 21:38:27 +0100

    incremento e decremento

--- 03-operatori/assegnazione.py
+++ 03-operatori/assegnazione.py
@@ -5,3 +5,26 @@ eta += 5
 eta = eta + 5
 eta -= 5
 print(eta)
+
+# Operatori incrementali e decrementali
+year = 2021
+
+#Incremento
+#year = year + 1
+year += 1
+
+#Decremento
+#year = year - 1
+year -= 1
+
+#Pre incremento
+year = 1 + year
+
+#Pre decremento
+year = 1 - year
+
+print(year)
+"""
+in python non esiste 
+variable++ o variable--
+"""
