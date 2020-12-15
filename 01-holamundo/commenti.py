a49c3c703beb1d358f7beabfa512273a5ce8f0b9
Adrian Medina <medinalarry96@gmail.com>
2020-12-15 13:23:55 +0100

    commenti

---
+++ 01-holamundo/commenti.py
@@ -0,0 +1,14 @@
+# Questo mostra testo Ciao nella terminale
+print("Ciao")
+
+# Quest'istruzione mostra testo nella terminale
+print("Mondo")
+#print("!!")
+
+"""
+Questo non si mostrerà:
+    print("Questo non si mostrera")
+    print("Commenti multilinea")
+"""
+
+print("Addio!!")
