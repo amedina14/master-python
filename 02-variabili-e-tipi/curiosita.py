0eb344eb78a3ccfbc4aa5a25b1fbf916cfc80be1
Adrian Medina <medinalarry96@gmail.com>
2020-12-15 19:24:58 +0100

    tips sulle variabili e stringhe

---
+++ 02-variabili-e-tipi/curiosita.py
@@ -0,0 +1,15 @@
+mio_testo = '\"Master\"'
+mio_testo2 = "in \"Python\""
+#I nomi delle variabili non possono avere i caratteri delle operazioni matematiche. Es: '-'.
+
+testo_unito = mio_testo + " " + mio_testo2
+print(testo_unito)
+
+testo_unito = mio_testo + "\n" + mio_testo2
+print(testo_unito)
+
+testo_unito = mio_testo + "\t" + mio_testo2
+print(testo_unito)
+
+testo_unito = mio_testo + "\r" + mio_testo2
+print(testo_unito)
