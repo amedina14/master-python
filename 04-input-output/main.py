1d96cc90260b69ae10a17f60cdcf5d1e2bf5476f
Adrian Medina <medinalarry96@gmail.com>
2020-12-15 21:59:08 +0100

    input e out da console

---
+++ 04-input-output/main.py
@@ -0,0 +1,7 @@
+#Input
+nome = input("Quale è il tuo nome? ")
+eta = int(input("Quale è la tua età? ")) #Converte a int il dato ricevuto da input
+
+#Output
+print(f"Ciao {nome}!. Vedo che hai {eta+2} anni") 
+#eta + 2 rappresenta int + int perciò si può fare
