26456f715d6821879398cbd41a8ed7f98c0d8747
Adrian Medina <medinalarry96@gmail.com>
2020-12-15 14:31:25 +0100

    variabile

---
+++ 02-variabili-e-tipi/variabili.py
@@ -0,0 +1,22 @@
+"""
+Una variabile è un contenitore d'informazione
+che dentro salva un dato, si possono creare molte
+variabili e ciascuna che abbia un dato distinto.
+"""
+
+variabile = 1
+variabile = "Master in Python"
+testo2 = "Udemy"
+numero = 45
+decimale = 6.7
+
+print(variabile)
+print(testo2)
+print(numero)
+print(decimale)
+
+print("--------------")
+numero = 77
+decimale = 8.9
+print(numero)
+print(decimale)
