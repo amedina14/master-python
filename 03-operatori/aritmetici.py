80c02eb8d845fd7b15f9a09438b94a0404a32dbf
Adrian Medina <medinalarry96@gmail.com>
2020-12-15 19:53:22 +0100

    operatori aritmetici

---
+++ 03-operatori/aritmetici.py
@@ -0,0 +1,15 @@
+#Operatori aritmetici
+numero1 = 77
+numero2 = 44 # operatore assegnazione =
+
+resta = numero1 - numero2
+moltiplicazione = numero1 * numero2
+divisione = numero1 / numero2
+resto = numero1 % numero2
+
+print("***********CALCOLATRICE*************")
+print(f"La suma è: {numero1 + numero2} ")
+print(f"La resta è: {resta} ")
+print(f"La moltiplicazione è: {moltiplicazione} ")
+print(f"La divisione è: {divisione} ")
+print("Il modulo o resto è:",resto)
