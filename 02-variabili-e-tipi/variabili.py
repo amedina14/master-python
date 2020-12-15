c7bf5df99a3daeeb5ecedf02e4e1508256f58712
Adrian Medina <medinalarry96@gmail.com>
2020-12-15 16:29:58 +0100

    concatenazione

--- 02-variabili-e-tipi/variabili.py
+++ 02-variabili-e-tipi/variabili.py
@@ -4,19 +4,36 @@ che dentro salva un dato, si possono creare molte
 variabili e ciascuna che abbia un dato distinto.
 """
 
+print("------------------------------------")
+
+#Creare variabili e assegnare un valore
 variabile = 1
-variabile = "Master in Python"
-testo2 = "Udemy"
-numero = 45
-decimale = 6.7
+titolo = "Master in Python"
+istituzione = "Udemy"
+qualificazione = 45
+durataOre = 6.7
 
-print(variabile)
-print(testo2)
-print(numero)
-print(decimale)
+#Mostrare valori
+print(f"titolo {titolo}")
+print(f"istituzione {istituzione}")
+print(f"qualificazione {qualificazione}")
+print(f"durataOre {durataOre}")
 
-print("--------------")
-numero = 77
-decimale = 8.9
-print(numero)
-print(decimale)
+print("------------------------------------")
+
+#Sostituire il valore di alcune variabili / riassegnando valori
+qualificazione = 77
+durataOre = 8.9
+print(f"qualificazione {qualificazione}")
+print(f"durataOre {durataOre}")
+
+print("------------------------------------")
+nome = "Adrian"
+cognome = "Medina"
+web = "arandom.000webhostapp.com"
+
+print(nome +" "+ cognome +" "+ web)
+print(f"{nome} {cognome} - {web}") #f formatta contenuto/testo ed interpola variabili.
+print("Ciao, mi chiamo {} {} e la mia web è {}".format(nome,cognome,web))
+print(nome, cognome, web)
+print("------------------------------------")
