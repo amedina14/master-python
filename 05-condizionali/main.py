5965920b24b0292b437584477b4d046cc20656a3
Adrian Medina <medinalarry96@gmail.com>
2020-12-16 12:31:59 +0100

    operatori di comparazione

--- 05-condizionali/main.py
+++ 05-condizionali/main.py
@@ -5,9 +5,18 @@ if true:
     instruc.
 else false:
     instruc.
+
+#operatori di comparazione
+==
+!=
+<
+>
+<=
+>=
+
 """
 
-# Ejemplo 1
+# Esempio 1
 print("######### ESEMPIO 1 #########")
 color = "rojo"
 if color == "rojo":
@@ -15,3 +24,12 @@ if color == "rojo":
     print(f"color {color}")
 else:
     print("Not red")
+
+# Esempio 2
+print("\n######### ESEMPIO 2 #########")
+year = 2020
+year = int(input("In che anno siamo? 5"))
+if year >= 2021:
+    print("Anno maggiore uguale al 2021 in avanti")
+else:
+    print("Siamo nel periodo che va fino all'anno 2020")
