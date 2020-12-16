23767042b562cca4d68bc5b827a9a26e69c2fb7b
Adrian Medina <medinalarry96@gmail.com>
2020-12-16 09:52:51 +0100

    condizione if

---
+++ 05-condizionali/main.py
@@ -0,0 +1,17 @@
+"""
+# Condizione if
+
+if true:
+    instruc.
+else false:
+    instruc.
+"""
+
+# Ejemplo 1
+print("######### ESEMPIO 1 #########")
+color = "rojo"
+if color == "rojo":
+    print("Trovato")
+    print(f"color {color}")
+else:
+    print("Not red")
