56ee78fdbc512b1daa2b8da7d98d37776210a131
Adrian Medina <medinalarry96@gmail.com>
2020-12-17 15:57:02 +0100

    tabella di moltiplicazione

--- 06-bucles/bucle-for.py
+++ 06-bucles/bucle-for.py
@@ -12,3 +12,24 @@ for i in range(0,10):
     #somma=c+i
     somma+=i
 print("somma totale",somma)
+
+# Esempio tabella moltiplicazone
+print("\n############# Esempio: tabella moltiplicazone #############")
+
+numero_utente = int(input("Di quale tabella vuoi il numero? "))
+
+if numero_utente < 1:
+    print("numero minore di 1")
+    numero_utente = 1
+
+
+print(f"### tabella del {numero_utente} ###")
+for i in range(1,10):
+
+    if numero_utente == 10:
+        print("numero non permesso")
+        break
+
+    print(f"{numero_utente}x{i}: "+str(numero_utente*i))
+else:
+    print("Tabella finita")
