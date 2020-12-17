be2034924a01d0eb50ca1ca63941fc574e36767d
Adrian Medina <medinalarry96@gmail.com>
2020-12-17 17:46:52 +0100

    esempi bucle while

---
+++ 06-bucles/bucle-while.py
@@ -0,0 +1,27 @@
+print("------------------------")
+i=1
+stampa = str(0)
+
+while i<=5:
+    print(i)
+    i+=1
+
+print("------------------------")
+i=1
+stampa = str(0)
+
+while i<=5:
+    stampa = stampa+", "+str(i)
+    #print(i)
+    i+=1
+print(stampa)
+
+print("------------------------")
+i=1
+colonna = str(0)
+
+while i<=5:
+    colonna = colonna+", "+str(i)
+    print(colonna)
+    i+=1
+
