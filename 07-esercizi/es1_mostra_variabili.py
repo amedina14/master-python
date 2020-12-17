50382ba27608f14aec85b810a276a9835b75e53f
Adrian Medina <medinalarry96@gmail.com>
2020-12-17 20:40:14 +0100

    esercizio 1

---
+++ 07-esercizi/es1_mostra_variabili.py
@@ -0,0 +1,13 @@
+"""
+Esercizio 1:
+    - Creare due variabili un "paese" e un altra "continente"
+    - Mostrare il suo valore (stampare)
+    - Inserire come commento il tipo di dato  
+"""
+
+paese = "Ecuador"       #string
+continente = "America"  #string
+year = 2021
+
+print(f"{paese} - {continente} - {str(year)}")
+print(type(paese), type(continente), type(year))
