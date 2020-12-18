5bcfbbe20dee76338bc10955323b05bafea6bfc8
Adrian Medina <medinalarry96@gmail.com>
2020-12-18 21:47:33 +0100

    esercizio 9

---
+++ 07-esercizi/es9_stop_fino_a_ins_N.py
@@ -0,0 +1,9 @@
+"""
+Esercizio 9
+programma che chiede e stampa numeri fino all'inserimento del numero 111
+"""
+numero = 0
+
+while numero != 111:
+    numero = int(input("Inserisci numero: "))
+    print(numero)
