2a92b7d271ad91b161b8c831dc0a34aedcb4668a
Adrian Medina <medinalarry96@gmail.com>
2020-12-19 11:48:06 +0100

    funzione e dieci tabelline

--- 08-funzioni/main.py
+++ 08-funzioni/main.py
@@ -34,3 +34,38 @@ def nome_controlla_eta(n,e):
 nome = input("\nInserisci nome: ")
 eta = int(input("Inserisci eta: "))
 nome_controlla_eta(nome,eta)
+
+#Esempio 3 tabellina con parametro
+print("\n##### Esempio 3 #####")
+
+def tabellina(n):
+    print(f"\n### Tabellina del {n} ###")
+    for i in range(1,11):
+        print(f"{i} x {n}: {i*n}")
+
+numero = int(input("Vuoi la tabellina del: "))
+tabellina(numero)
+
+#Esempio 3.1 tabellina con parametro
+for j in range(1,11):
+    tabellina(j)
+
+""""
+print("\n##### Esempio 3.1 #####")
+
+def tabelline():
+    print(f"### 10 Tabelline ###")
+    for i in range(1,11):
+        for ii in range(1,11):
+            print(f"{i} x {ii}: {i*ii}")
+        print()
+#numero = int(input("Vuoi la tabellina del: "))
+tabelline()
+"""
+
+"""
+def dieci_tabelline():
+    for j in range(1,11):
+        tabellina(j)
+dieci_tabelline()
+"""
