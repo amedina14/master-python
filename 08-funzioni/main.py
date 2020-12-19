de6d1230911b56a6a879c14158591721763f4d60
Adrian Medina <medinalarry96@gmail.com>
2020-12-19 10:44:21 +0100

    Creare funzione

---
+++ 08-funzioni/main.py
@@ -0,0 +1,19 @@
+"""
+Insieme di istruzioni sotto un nome concreto che si possono riutilizare
+invocando il suo nome quante volte si voglia
+"""
+
+#Esempio 1
+
+def incremento():
+    print("Hello")
+    print("Ciao")
+    print("Hola")
+    print("Halo")
+    print("Salut")
+    print()
+
+# Invocare funzione
+incremento()
+incremento()
+incremento()
