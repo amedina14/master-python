b08a895eeb19b1c4233b667959f0250c0ebe8c9b
Adrian Medina <medinalarry96@gmail.com>
2020-12-19 10:59:18 +0100

    esempio 2, parametri

--- 08-funzioni/main.py
+++ 08-funzioni/main.py
@@ -4,7 +4,7 @@ invocando il suo nome quante volte si voglia
 """
 
 #Esempio 1
-
+print("##### Esempio 1 #####")
 def incremento():
     print("Hello")
     print("Ciao")
@@ -17,3 +17,20 @@ def incremento():
 incremento()
 incremento()
 incremento()
+
+#Esempio 2 parametri
+print("##### Esempio 2 #####")
+def ciclo(parola):
+    for i in range(10):
+        print(parola,i,end="; ")
+
+ciclo("Iterazione")
+
+def nome_controlla_eta(n,e):
+    print(f"Ti chiami {n}")
+    if e >= 18:
+        print("Sei maggiorenne")
+
+nome = input("\nInserisci nome: ")
+eta = int(input("Inserisci eta: "))
+nome_controlla_eta(nome,eta)
