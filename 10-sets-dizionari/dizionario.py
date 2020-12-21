673ea7424a1cc42317860c1683ed2b69428799ad
Adrian Medina <medinalarry96@gmail.com>
2020-12-21 18:10:17 +0100

    dizionari

---
+++ 10-sets-dizionari/dizionario.py
@@ -0,0 +1,22 @@
+"""
+Dizionario
+Un tipo di collezione che memorizza dati in formato
+chiave > valore.
+Simile ad un array associativo o ad un oggetto json
+"""
+
+persona = {
+    "nome": "Adrian",
+    "paese": "Ecuador",
+    "web": "arandom.000webhostapp.com"
+}
+print(type(persona))
+print(persona)
+print("\n# Singolo valore per chiave nome #\n",persona["nome"])
+
+print("\nChiavi:", end="\t ")
+for dato in persona:
+    print(dato, end="\t ")
+print("\nValori:", end="\t ")
+for dato in persona:
+    print(persona[dato], end="\t ")
