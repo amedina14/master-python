a1fe43978570647953f2b89a6a895e8707922dd1
Adrian Medina <medinalarry96@gmail.com>
2020-12-21 13:52:31 +0100

    Scorrere liste, ed aggiungere elemento

--- 09-liste/main.py
+++ 09-liste/main.py
@@ -4,6 +4,7 @@ Per accedere o manipolare dati al suo intero dobbiamo usare un indice numerico.
 """
 
 # Definire lista
+print("\n#### Definire liste ####")
 films = ["Batman","Superman","Lo Hobbit", "Star Wars"]
 
 # Se vogliamo usare il metodo 'list', gli dobbiamo passare una tupla.
@@ -27,7 +28,38 @@ films[1] = "Gran Torino"
 print(films)
 
 # Aggiungere elementi alle liste
-
+print("\n#### Aggiungere elementi ####")
 cinemas.append("Cine Rock Star")
 cinemas.append("CineStellar")
 print(cinemas)
+
+# Scorrere la lista
+print("\n#### Scorrere lista ####")
+def add_film():
+    new_film = ""
+    risposta = ""
+    risposta = input("Vuoi aggiungere un nuovo film (s/n): ")
+    risposta = risposta.lower() #conversione risposta a minuscola
+    while risposta == "s":
+        new_film = input("Aggiungi nome del film: ")
+        films.append(new_film)
+        return films
+
+i = 0
+for i in films:
+    print(f"{films.index(i)+1}. {i}")
+
+add_film()
+for i in films:
+    print(f"{films.index(i)+1}. {i}")
+
+print("\nLoop per inserire films. Stop con \"exit\"")
+film = ""
+while film != "exit":
+    film = input("Insert film name: ")
+    if film != "exit":
+        films.append(film)
+
+for i in films:
+    print(f"{films.index(i)+1}. {i}")
+
