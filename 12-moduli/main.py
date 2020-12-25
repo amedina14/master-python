41a5a27f7c8e620cef8fe75e82ad0b6844bda7e0
Adrian Medina <medinalarry96@gmail.com>
2020-12-25 23:40:36 +0100

    main richiama modulo

---
+++ 12-moduli/main.py
@@ -0,0 +1,26 @@
+"""
+Moduli
+funzionalità già fatte riutilizzabili
+
+https://docs.python.org/3/py-modindex.html#cap-_
+
+-si possono includere metodi del linguaggio
+-si possono scaricare da internet
+-si possono creare i propri moduli
+"""
+
+""""
+import mio_modulo 
+
+mio_modulo.saluta("Adrian")
+"""
+
+"""
+from mio_modulo import saluta
+
+print(saluta("Adrian"))
+"""
+
+from mio_modulo import *
+
+print(saluta("Adrian"))
