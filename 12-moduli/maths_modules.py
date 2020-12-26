5b8d491372523cf49b4dea12823961aaa8e9815e
Adrian Medina <medinalarry96@gmail.com>
2020-12-26 13:28:30 +0100

    math module

---
+++ 12-moduli/maths_modules.py
@@ -0,0 +1,8 @@
+import math
+
+print("Radice quadrata di 25:", math.sqrt(25))
+print("Numeri pi:", float(math.pi))
+
+print("Arrotonda al successivo: ", math.ceil(5.01942))
+print("Arrotonda al precedente: ", math.floor(5.01942))
+
