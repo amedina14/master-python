f48b3b8b11580327dd9cbaca7e380f3fa8369e6a
Adrian Medina <medinalarry96@gmail.com>
2020-12-26 19:58:24 +0100

    Read file

--- 14-sistema-di-files/files.py
+++ 14-sistema-di-files/files.py
@@ -12,11 +12,41 @@ i=0
 flag = True
 while flag == True:
     i+=1
-    file.write(f"*******Stringa {i} del file********\n")
-    print(f"*******Stringa {i} del file********\n")
+    file.write(f"******* Stringa {i} del file ********\n")
+    print(f"Riga aggiunta al file: ******* stringa {i} del file ********")
     if i == 10:
         #break
         flag = False
+print(
 
+)
 #Chiudere file
 file.close()
+
+#Aprire file in modo lettura
+file_read = open(path, "r")
+#leggere file
+#contenuto = file_read.read()
+#print("Lettura file","\n"+contenuto)
+"""
+#Stampa lettera per lettera di ciascuna riga
+for i in contenuto:
+    print(i, end="")
+"""
+
+#Leggere contenuto e salvare in lista
+lista = file_read.readlines()
+file_read.close()
+#print(lista)
+print("Lettura file")
+for riga in lista:
+    """
+    if not riga.isnumeric():
+        print(riga.upper())
+    """
+    lista_riga = riga.split()
+    print(lista_riga)
+    print(riga.capitalize().center(50)) #prima lettera della riga in maiuscola
+    #print(riga.center(50)) 
+    #aggiunge 50 spazzi ai fianchi per centrare
+    
