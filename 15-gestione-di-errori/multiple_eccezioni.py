a61a9f02336615f7e1f77daade5a6bc815475d6f
Adrian Medina <medinalarry96@gmail.com>
2020-12-28 19:39:47 +0100

    eccezioni multiple e trovare il tipo di eccezione

---
+++ 15-gestione-di-errori/multiple_eccezioni.py
@@ -0,0 +1,18 @@
+ripeti = True
+while ripeti == True:
+    ripeti = False
+    try:
+        numero = int(input("inserisci un numero per fare il quadrato: "))
+        print(f"Il quadrato è {numero*numero}")
+    except TypeError:
+        print("Errore: il valore è stringa, non può fare il quadrato")
+        ripeti = True
+    except Exception as e:
+        print("C'è stato un errore:", type(e).__name__)
+        print(type(e))
+        print(e)
+    except ValueError:
+        print("Il valore inserito non è numerico")
+        ripeti = True
+    except KeyboardInterrupt:
+        print("\nCiao, alla prossima!")
