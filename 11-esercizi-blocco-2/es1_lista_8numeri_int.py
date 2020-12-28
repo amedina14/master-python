7f1aab3261aaa25fc740a5f697a3b6bae67155b6
Adrian Medina <medinalarry96@gmail.com>
2020-12-28 18:51:52 +0100

    aggiunte eccezioni per la ricerca del valore

--- 11-esercizi-blocco-2/es1_lista_8numeri_int.py
+++ 11-esercizi-blocco-2/es1_lista_8numeri_int.py
@@ -9,11 +9,11 @@ Lista di 8 numeri interi
 
 numeri = [11,2,6,7,4,3,17,14]
 
-#1
+#1 scorrere e stamparla
 for numero in numeri:
     print(numero)
 
-#2
+#2 funzione che scorra la lista e restituisca un string
 def scorri(lista):
     risultato = ""
     for i in lista: # 'i' prende il valore dell'elemento
@@ -23,20 +23,23 @@ def scorri(lista):
     return risultato
 print("\n"+scorri(numeri))
 
-#3
+#3 ordinarla e stamparla
 numeri.sort()
 print(f"\nLista ordinata: {scorri(numeri)}")
 
-#4
+#4 mostrare lunghezza
 print(f"\nLunghezza della lista: {len(numeri)}")
 
-#5
-cerca = int(input("\nInserisci il numero da cercare: "))
-controlla = isinstance(cerca, int)
-while not controlla or cerca <= 0: 
+#5 Cercare elemento richiesto da tastiera
+try:
     cerca = int(input("\nInserisci il numero da cercare: "))
-else:
-    print(f"Inserito {cerca}")
-position = numeri.index(cerca)
-print("Il numero \""+ str(cerca) +"\" si trova nel indice: "+str(position))
+    controlla = isinstance(cerca, int)
+    while not controlla or cerca <= 0: 
+        cerca = int(input("\nInserisci il numero da cercare: "))
+    else:
+        print(f"Inserito {cerca}")
+    position = numeri.index(cerca)
+    print("Il numero \""+ str(cerca) +"\" si trova nel indice: "+str(position))
+except:
+    print("Il valore non è presente")
 
