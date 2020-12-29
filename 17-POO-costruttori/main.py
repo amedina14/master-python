658dbd6d6b1251df52793101bfbaa3cb5d76f696
Adrian Medina <medinalarry96@gmail.com>
2020-12-29 16:07:27 +0100

    lista di oggetti macchine

---
+++ 17-POO-costruttori/main.py
@@ -0,0 +1,26 @@
+from veicolo import Veicolo
+
+macchina1 = Veicolo("blu","Lamborghini","Murcielago",450,500,2)
+macchina2 = Veicolo("rosso","Ferrari","LaFerrari",450,500,2)
+macchina3 = Veicolo("giallo","Jeep","Wrangler",280,250,4)
+macchina4 = Veicolo("griggio","Ferrari","Roma",450,500,2)
+
+"""
+"""
+lista_macchine = [
+    macchina1,
+    macchina2,
+    macchina3,
+    macchina4
+]
+
+#macchina1.setMarca(input("Inserisci marca della macchina 1: "))
+#print(macchina1.getMarca())
+
+for macchina in lista_macchine:
+    print(f"---------Informazione veicolo {lista_macchine.index(macchina)+1}---------")
+    print(lista_macchine[lista_macchine.index(macchina)].getInfo())
+
+#print(macchina1.getInfo())
+#print(lista_macchine[0].getInfo())
+#print(macchina1.getInfo())
