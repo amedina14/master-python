0e8875271c55b09b3d572c73cc86ad5e68d5d7d8
Adrian Medina <medinalarry96@gmail.com>
2020-12-30 23:53:41 +0100

    classe persona e informatico

---
+++ 18-POO-ereditarieta/classi.py
@@ -0,0 +1,66 @@
+"""
+Ereditarieta:
+Possibilita di condividere attributi e metodi tra le classi.
+E che diversi classi eredino da altri classi.
+"""
+
+class Persona:
+    """
+    nome
+    cognome
+    altezza
+    eta
+    """
+
+    def getNome(self):
+        return self.nome    
+    def setNome(self, nome):
+        self.nome = nome
+    
+    def getCognome(self):
+        return self.cognome    
+    def setCognome(self, cognome):
+        self.cognome = cognome
+    
+    def getAltezza(self):
+        return self.altezza
+    def setAltezza(self, altezza):
+        self.altezza = altezza
+    
+    def getEta(self):
+        return self.eta
+    def setEta(self, eta):
+        self.eta = eta
+
+    def parlare(self):
+        return "Sto parlando"
+    
+    def caminare(self):
+        return "Sto caminando"
+    
+    def dormire(self):
+        return "Buona notte. Dormendo"
+
+class Informatico(Persona):
+    """
+    linguaggi
+    esperienza
+    progetti
+    """
+
+    def __init__(self): # Costruttore che aggiunge automaticamente valori alle proprieta
+        self.linguaggi = "Python, Java, C#, JavaScript"
+        self.esperienza = 5
+    
+    def getLinguaggi(self):
+        return self.linguaggi
+    
+    def imparare(self, linguaggi):
+        self.linguaggi = linguaggi
+        return self.linguaggi
+    
+    def programmare(self):
+        return "Sto programmando"
+    
+    def ripararePc(self):
+        return "Ho riparato il tuo pc"
