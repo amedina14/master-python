0d374f075c9fbeec9cce1974d73a8c61f38de335
Adrian Medina <medinalarry96@gmail.com>
2021-01-04 20:16:44 +0100

    Testo della specifica

---
+++ 20-applicazione-console-notes-list/main.py
@@ -0,0 +1,9 @@
+"""
+Applicazione di console 'Lista appunti' Python e Mysql:
+- Apre assistente
+- Login o registro
+- Se scegliamo registro, crea un utente nel DB
+- Se scegliamo login, identifica l'user chiedendo le credenziali
+- Crea, legge, e cancella appunti. 
+"""
+
