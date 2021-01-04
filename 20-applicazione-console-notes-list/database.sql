939010950a95472463c5533b1fd7d87c3ab537bf
Adrian Medina <medinalarry96@gmail.com>
2021-01-04 22:05:42 +0100

    database and tables

---
+++ 20-applicazione-console-notes-list/database.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS master_python;
+use master_python;
+
+CREATE TABLE IF NOT EXISTS users(
+id          int(25) auto_increment not null,
+nome        varchar(30) not null,
+cognome     varchar(30) not null,
+email       varchar(60) not null,
+password    varchar(60) not null,
+data        date not null,
+CONSTRAINT pk_users PRIMARY KEY(id),
+CONSTRAINT uq_email UNIQUE(email)
+)ENGINE=InnoDb;
+
+CREATE TABLE IF NOT EXISTS notes(
+id          int(25) auto_increment not null,
+user_id     int(25) not null,
+titulo      varchar(30) not null,
+contenuto   MEDIUMTEXT,
+data        date not null,
+CONSTRAINT pk_users PRIMARY KEY(id),
+CONSTRAINT fk_nota_user FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=InnoDb;
