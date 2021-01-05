d494a7e616a77e5487ad50c92b699ec9c513dc1e
Adrian Medina <medinalarry96@gmail.com>
2021-01-05 17:29:43 +0100

    domande per l'utente

--- 20-applicazione-console-notes-list/main.py
+++ 20-applicazione-console-notes-list/main.py
@@ -6,7 +6,17 @@ Applicazione di console 'Lista appunti' Python e Mysql:
 - Se scegliamo login, identifica l'user chiedendo le credenziali
 - Crea, legge, e cancella appunti. 
 """
+import mysql.connector
 
+connessione = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "",
+    port = 3308,
+    database = "master_python"
+)
+
+cursor = connessione.cursor()
 
 print("""
 Azioni disponibili:
@@ -14,14 +24,38 @@ Azioni disponibili:
     -Login
 """)
 
-azione = input("Inserisci la tua scelta: ").capitalize()
+try:
+    azione = input("Inserisci la tua scelta: ").capitalize()
 #print(f"'{azione}'")
+except:
+    print("Errore: I dati inseriti non sono corretti")
 
 if azione == "Registro":
     print("Registro utente selezionato. ")
 
+    try:
+        regs_nome = input(f"Registra nome utente:")
+        regs_cognome = input(f"Registra cognome utente:")
+        regs_email = input(f"Registra email utente:")
+        regs_pwd = input(f"Registra password utente:")
+    except:
+        print("Errore: I dati inseriti non sono corretti")
+
+    print(f"""
+        Utente registrato:
+        -{regs_nome}
+        -{regs_cognome}
+        -{regs_email}
+    """)
+
 elif azione == "Login":
     print("Login selezionato. Entra nel sistema: ")
 
-    #ins_email= input("Inserisci email: ")
-    #ins_pwd= input("Inserisci password: ")
+    try:
+        log_email = input(f"Inserisci email utente:")
+        log_pwd = input(f"Inserisci password utente:")
+    except:
+        print("Errore: I dati inseriti non sono corretti")
+
+
+connessione.close()
