303e24520426b3955ec3119f26ba0fbb6a4e8f31
Adrian Medina <medinalarry96@gmail.com>
2021-01-08 14:47:52 +0100

    implementazione metodo autenticare utente col db

--- 20-applicazione-console-notes-list/utenti/utente.py
+++ 20-applicazione-console-notes-list/utenti/utente.py
@@ -3,7 +3,7 @@ Connessione a DB e classe utente
 """
 import datetime
 import hashlib
-import utenti.connessione as conn
+from utenti import connessione as conn
 
 connect = conn.connettere()
 database = connect[0]
@@ -20,9 +20,9 @@ class Utente:
     def registrare(self):
         data = datetime.datetime.now()
 
-        # codifica password
+        # Codifica password
         crittografia = hashlib.sha256()
-        crittografia.update(self.password.encode('utf-8'))
+        crittografia.update(self.password.encode('utf8'))
 
         sql = "INSERT INTO users VALUES (null, %s,%s,%s,%s,%s)"
         user = (self.nome,self.cognome,self.email,crittografia.hexdigest(),data)
@@ -37,7 +37,36 @@ class Utente:
         return result
     
     def autenticare(self):
-        return self.nome
+
+        # Domanda se esiste l'utente
+        sql = "SELECT * FROM users WHERE email=%s AND password=%s"
+
+        # Codifica password
+        crittografia = hashlib.sha256()
+        crittografia.update(self.password.encode('utf8'))
+
+        # Dati per la domanda
+        dati_sql = (self.email, crittografia.hexdigest())
+
+        cursor.execute("SELECT * FROM users WHERE email=%s AND password=%s", dati_sql)
+        dati_utente = cursor.fetchone()
+
+        for dato in dati_utente:
+            if dati_utente.index(dato) == 0:
+                print(f"Id: {dato}")
+            elif dati_utente.index(dato) == 1:
+                print(f"Nome: {dato}")
+            elif dati_utente.index(dato) == 2:
+                print(f"Cognome: {dato}")
+            elif dati_utente.index(dato) == 3:
+                print(f"Email: {dato}")
+            elif dati_utente.index(dato) == 4:
+                print(f"Password: {dato}")
+            elif dati_utente.index(dato) == 5:
+                print(f"Data creazione: {dato}")
+        print()
+        
+        return dati_utente
 
 """
 In questo punto la chiusura della connessione non permetteva di usare il DB
