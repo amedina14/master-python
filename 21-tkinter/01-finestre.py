51b2f2654ad0556bb0c5adf057ed3bfec9002409
Adrian Medina <medinalarry96@gmail.com>
2021-01-11 20:05:25 +0100

    migliorando il programma usando classi ed oop

--- 21-tkinter/01-finestre.py
+++ 21-tkinter/01-finestre.py
@@ -5,30 +5,65 @@ Modulo di python per creare interfacce grafiche di utenti
 import os.path # as pathlib
 from tkinter import *
 
-# Creare la finestra principale
-window = Tk(screenName="First window with tk")
+class Programma:
 
-# Titolo della finestra
-window.title("Prima app Python GUI")
+    def __init__(self):
+        self.titolo = "Prima app Python GUI"
+        self.percorso = "./img/logo-tesseract.ico"
+        self.percorso_alt = "./21-tkinter/img/logo-tesseract.ico"
+        self.dimensione = "750x450"
+        self.resizable = False
 
-# Verificare l'esistena di un file
-indirizzo_icono = os.path.abspath("./img/logo-tesseract.ico")
+    def carica(self):
+        # Creare la finestra principale
+        window = Tk(screenName="First window with tk")
+        self.window = window
 
-if not os.path.isfile(indirizzo_icono): # Se non trova l'indireizzo dell'immagine
-    indirizzo_icono = os.path.abspath("./21-tkinter/img/logo-tesseract.ico")    
+        # Titolo della finestra
+        window.title(self.titolo)
 
-# Icono della finestra
-window.iconbitmap(indirizzo_icono)
+        # Verificare l'esistena di un file
+        indirizzo_icono = os.path.abspath(self.percorso)
 
-# Mostrare testo in una label
-label_testo = Label(window, text=indirizzo_icono)
-label_testo.pack()
+        if not os.path.isfile(indirizzo_icono): # Se non trova l'indireizzo dell'immagine
+            indirizzo_icono = os.path.abspath(self.percorso_alt)
 
-# Cambi nelle dimensioni della finestra
-window.geometry("750x450")
+        # Icono della finestra
+        window.iconbitmap(indirizzo_icono)
 
-# Bloccare il ridimensionamento della finestra: fx(x,y) => 0 bloccato, 1 sbloccato
-window.resizable(1,0)
+        # Mostrare testo in una label
+        label_testo = Label(window, text=indirizzo_icono)
+        label_testo.pack()
 
-# Eseguire e chiamare la finestra fino alla chiusura, importante che questo metodo sia tra gli ultimi.
-window.mainloop()
+        # Cambi nelle dimensioni della finestra
+        window.geometry(self.dimensione)
+
+        # Bloccare il ridimensionamento della finestra: fx(x,y) => 0 bloccato, 1 sbloccato
+        if self.resizable:
+            window.resizable(1,0)
+        else:
+            window.resizable(0,0)
+
+    def add_text(self, dato):
+        testo = Label(self.window, text=dato)
+        testo.pack()
+
+    def stampa(self):
+        # Eseguire e chiamare la finestra fino alla chiusura, importante che questo metodo sia tra gli ultimi.
+        self.window.mainloop()
+
+# Instanziare programa
+programma = Programma()
+programma.carica()
+programma.add_text("Hello World 2021!")
+programma.add_text("I want to travel to warm places")
+programma.add_text("""Atene, 
+Mykonos, 
+Fira, 
+Salonicco, 
+Santorini, 
+La Canea, 
+Rodi, 
+Corfù, 
+Heraklio""")
+programma.stampa()
