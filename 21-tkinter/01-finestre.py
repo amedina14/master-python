5ecdd2ad1c57aacac38f7fd86a5eb6230145ec57
Adrian Medina <medinalarry96@gmail.com>
2021-01-11 18:03:14 +0100

    controllare il percorso img e renderlo accessibile

--- 21-tkinter/01-finestre.py
+++ 21-tkinter/01-finestre.py
@@ -2,7 +2,7 @@
 Tkinter
 Modulo di python per creare interfacce grafiche di utenti
 """
-
+import os.path # as pathlib
 from tkinter import *
 
 # Creare la finestra principale
@@ -11,8 +11,18 @@ window = Tk(screenName="First window with tk")
 # Titolo della finestra
 window.title("Prima app Python GUI")
 
+# Verificare l'esistena di un file
+indirizzo_icono = os.path.abspath("./img/logo-tesseract.ico")
+
+if not os.path.isfile(indirizzo_icono): # Se non trova l'indireizzo dell'immagine
+    indirizzo_icono = os.path.abspath("./21-tkinter/img/logo-tesseract.ico")    
+
 # Icono della finestra
-window.iconbitmap("./21-tkinter/img/logo-tesseract.ico")
+window.iconbitmap(indirizzo_icono)
+
+# Mostrare testo in una label
+label_testo = Label(window, text=indirizzo_icono)
+label_testo.pack()
 
 # Cambi nelle dimensioni della finestra
 window.geometry("750x450")
