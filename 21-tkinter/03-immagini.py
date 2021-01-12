7a95fd25ddb7bb24b22a3702b70ed5e1aaddc570
Adrian Medina <medinalarry96@gmail.com>
2021-01-12 19:57:35 +0100

    ridimensionare immagine: config a elemento render

--- 21-tkinter/03-immagini.py
+++ 21-tkinter/03-immagini.py
@@ -15,6 +15,11 @@ label1 = Label(finestra, text="VENV Inserire immagini dentro del programma:").pa
 immagine = Image.open('21-tkinter\img\logo-tesseract.png')
 renderizzare = ImageTk.PhotoImage(immagine)
 
-Label(finestra, image=renderizzare).pack(anchor=E)
+lbl_img = Label(finestra, image=renderizzare)
+lbl_img.config(
+    width=350,
+    heigh=350
+)
+lbl_img.pack(side=BOTTOM,anchor=CENTER)
 
 finestra.mainloop()
