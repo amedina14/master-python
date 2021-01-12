0a9778959da58429b828cbcf27471b9218351b14
Adrian Medina <medinalarry96@gmail.com>
2021-01-12 17:41:29 +0100

    caricare immagini con pillow

---
+++ 21-tkinter/03-immagini.py
@@ -0,0 +1,20 @@
+from tkinter import *
+#from Pillow import *
+from PIL import Image, ImageTk
+
+
+"""
+pip install --upgrade Pillow
+"""
+
+finestra = Tk()
+finestra.geometry("650x450")
+
+label1 = Label(finestra, text="VENV Inserire immagini dentro del programma:").pack(anchor=W)
+
+immagine = Image.open('21-tkinter\img\logo-tesseract.png')
+renderizzare = ImageTk.PhotoImage(immagine)
+
+Label(finestra, image=renderizzare).pack(anchor=E)
+
+finestra.mainloop()
