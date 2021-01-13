e5bcb6b740b09287dd008008ee4f2db3ec5a46f5
Adrian Medina <medinalarry96@gmail.com>
2021-01-13 17:06:50 +0100

    campi di testo in forms con grid

--- 21-tkinter/06-forms.py
+++ 21-tkinter/06-forms.py
@@ -1,9 +1,14 @@
 from tkinter import *
 
+"""
+Se si usa Grid non si può usare pack sullo stesso elemento/i anche acendo relazione.
+"""
+
 finestra = Tk()
 finestra.geometry("700x400")
 finestra.title("Forms con Tkinter")
 
+# Header
 header = Label(finestra, text="Moduli con tkinter: Header")
 header.config(
     fg="white",
@@ -12,6 +17,15 @@ header.config(
     padx=10,
     pady=10
 )
-header.pack(side=LEFT, anchor=NW)
+header.grid(row=0, column=0, columnspan=12) # pack(side=LEFT, anchor=NW) # fill=Y, expand=YES
+
+# Label
+lbl_text = Label(finestra, text="Label form example")
+lbl_text.grid(row=1, column=0, sticky=W, padx=5, pady=5)
+
+# Textbox
+text_in = Entry(finestra)
+text_in.grid(row=1, column=1, sticky=W, padx=5, pady=5)
+text_in.config(justify="right", state="normal")
 
 finestra.mainloop()
