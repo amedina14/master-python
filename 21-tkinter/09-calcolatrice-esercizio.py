f67f512d952879faacfac810d9c537ad65186673
Adrian Medina <medinalarry96@gmail.com>
2021-01-14 21:47:03 +0100

    operazioni della calcolatrice

--- 21-tkinter/09-calcolatrice-esercizio.py
+++ 21-tkinter/09-calcolatrice-esercizio.py
@@ -12,6 +12,32 @@ finestra.geometry("700x450")
 finestra.config(bd="25")
 finestra.title("Calcolatrice")
 
+def somma(val1, val2):
+    risultato.set(float(val1.get()) + float(val2.get()))
+    mostraRisultato()
+    #return result
+
+def resta(val1, val2):
+    risultato.set(float(val1.get()) - float(val2.get()))
+    mostraRisultato()
+    #return result
+
+def molt(val1, val2):
+    risultato.set(float(val1.get()) * float(val2.get()))
+    mostraRisultato()
+    #return result
+
+def div(val1, val2):
+    risultato.set(float(val1.get()) / float(val2.get()))
+    mostraRisultato()
+    #return result
+
+def mostraRisultato():
+    messagebox.showinfo("Risultato", f"Risultato: {risultato.get()}")
+    # Svuota i textbox
+    numero1.set("")
+    numero2.set("")
+
 numero1 = StringVar()
 numero2 = StringVar()
 risultato = StringVar()
@@ -20,7 +46,7 @@ frame_superiore = Frame(finestra)
 frame_superiore.config(
     #font=("Arial",10),
     #bg="dodger blue",
-    width=250,
+    width=350,
     height=250,
     relief=SOLID,
     padx=10,
@@ -29,8 +55,6 @@ frame_superiore.config(
 )
 frame_superiore.pack(anchor=N) #grid(row=0, column=0, columnspan=3, anchor=CENTER)
 frame_superiore.pack_propagate(False)
-"""
-"""
 
 primo_valore = Label(frame_superiore, text="valore 1: ")
 primo_valore.pack()#grid(row=0,column=1)
@@ -45,18 +69,11 @@ in_valore2.pack()#grid(row=0,column=4, padx=5, pady=5)
 Label(frame_superiore, text="").pack()
 
 # Bottoni
-"""
-frame_operazioni = Frame(finestra)
-frame_operazioni.config(
-    bg="#ADFF2F"
-)
-frame_operazioni.pack() # grid(row=1,column=0)
-"""
 
-Button(frame_superiore, text="Somma").pack(side="left", fill=X, expand=YES)
-Button(frame_superiore, text="Resta").pack(side="left", fill=X, expand=YES)
-Button(frame_superiore, text="Moltiplicazione").pack(side="left", fill=X, expand=YES)
-Button(frame_superiore, text="Divisione").pack(side="left", fill=X, expand=YES)
+Button(frame_superiore, text="Somma", command=lambda: somma(numero1,numero2)).pack(side="left", fill=X, expand=YES)
+Button(frame_superiore, text="Resta", command=lambda: resta(numero1,numero2)).pack(side="left", fill=X, expand=YES)
+Button(frame_superiore, text="Moltiplicazione", command=lambda: molt(numero1,numero2)).pack(side="left", fill=X, expand=YES)
+Button(frame_superiore, text="Divisione", command=lambda: div(numero1,numero2)).pack(side="left", fill=X, expand=YES)
 
 
 # Mostra risultato in una alerta
