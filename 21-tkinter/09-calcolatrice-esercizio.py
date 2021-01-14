007fe723c49ca7c591d4d0da0f013d5ff60ecd56
Adrian Medina <medinalarry96@gmail.com>
2021-01-14 17:00:07 +0100

    Aggiunta di frames per posizionamento

--- 21-tkinter/09-calcolatrice-esercizio.py
+++ 21-tkinter/09-calcolatrice-esercizio.py
@@ -8,7 +8,7 @@ from tkinter import *
 from tkinter import messagebox
 
 finestra = Tk()
-#finestra.geometry("700x450")
+finestra.geometry("700x450")
 finestra.config(bd="25")
 finestra.title("Calcolatrice")
 
@@ -16,29 +16,33 @@ numero1 = StringVar()
 numero2 = StringVar()
 risultato = StringVar()
 
-"""
 frame_superiore = Frame(finestra)
 frame_superiore.config(
     #font=("Arial",10),
-    bg="dodger blue",
+    #bg="dodger blue",
+    width=250,
+    height=250,
+    relief=SOLID,
     padx=10,
     pady=10,
     bd=5,
 )
-frame_superiore.pack(anchor=N, fill=X, expand=YES) #grid(row=0, column=0, columnspan=3, anchor=CENTER)
+frame_superiore.pack(anchor=N) #grid(row=0, column=0, columnspan=3, anchor=CENTER)
+frame_superiore.pack_propagate(False)
+"""
 """
 
-primo_valore = Label(finestra, text="valore 1: ")
+primo_valore = Label(frame_superiore, text="valore 1: ")
 primo_valore.pack()#grid(row=0,column=1)
-in_valore1 = Entry(finestra, textvariable=numero1)
+in_valore1 = Entry(frame_superiore, textvariable=numero1, justify="center")
 in_valore1.pack()#grid(row=0,column=2, padx=5, pady=5)
 
-secondo_valore = Label(finestra, text="valore 2: ")
+secondo_valore = Label(frame_superiore, text="valore 2: ")
 secondo_valore.pack()#grid(row=0,column=3, padx=5, pady=5)
-in_valore2 = Entry(finestra, textvariable=numero2)
+in_valore2 = Entry(frame_superiore, textvariable=numero2, justify="center")
 in_valore2.pack()#grid(row=0,column=4, padx=5, pady=5)
 
-Label(finestra, text="").pack()
+Label(frame_superiore, text="").pack()
 
 # Bottoni
 """
@@ -49,10 +53,10 @@ frame_operazioni.config(
 frame_operazioni.pack() # grid(row=1,column=0)
 """
 
-Button(finestra, text="Somma").pack(side="left")
-Button(finestra, text="Resta").pack(side="left")
-Button(finestra, text="Moltiplicazione").pack(side="left")
-Button(finestra, text="Divisione").pack(side="left")
+Button(frame_superiore, text="Somma").pack(side="left", fill=X, expand=YES)
+Button(frame_superiore, text="Resta").pack(side="left", fill=X, expand=YES)
+Button(frame_superiore, text="Moltiplicazione").pack(side="left", fill=X, expand=YES)
+Button(frame_superiore, text="Divisione").pack(side="left", fill=X, expand=YES)
 
 
 # Mostra risultato in una alerta
