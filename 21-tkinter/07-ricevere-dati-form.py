fdc5ea555b92473ca498aae75eb377a0cf29a3f3
Adrian Medina <medinalarry96@gmail.com>
2021-01-14 12:51:37 +0100

    Riceve input, li processa e li stampa sul form

---
+++ 21-tkinter/07-ricevere-dati-form.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+finestra = Tk()
+finestra.geometry("700x450")
+finestra.config(
+    bg="CornflowerBlue", #6495ED
+    bd=50,
+    #highlightbackground="white"
+)
+
+
+def stampaDato():
+    datoRisultante.set(datoRaccolto.get())
+
+datoRaccolto = StringVar()
+datoRisultante = StringVar()
+
+# Inserisce dato
+Label(finestra, text="Inserisci testo: ").pack(anchor=NW)
+Entry(finestra, textvariable=datoRaccolto).pack(anchor=NW)
+
+# Mostra dato
+Label(finestra, text="Dato inserito").pack(anchor=NW)
+dato_stampato = Label(finestra, textvariable=datoRisultante)
+dato_stampato.config(
+    bg="navy",
+    fg="#00FF00"
+)
+dato_stampato.pack(anchor=NW)
+
+# Submit
+Label(finestra, background="#6495ED").pack(anchor=NW)
+Button(finestra, text="Invia", command=stampaDato).pack(anchor=NW)
+
+finestra.mainloop()
