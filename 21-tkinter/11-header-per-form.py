8dd832acdc5b537cfb11c44326791fa88235ff42
Adrian Medina <medinalarry96@gmail.com>
2021-01-15 23:31:20 +0100

    Combinazione if perfetta a 3 valori + checkbox

--- 21-tkinter/11-header-per-form.py
+++ 21-tkinter/11-header-per-form.py
@@ -12,6 +12,107 @@ header.config(
     bg="green",
     font=("Consolas",20)
 )
-header.pack(anchor=N, side=TOP, fill=X, expand=YES)
+header.grid(row=0,column=0,columnspan=5, sticky=W) #pack(anchor=N, side=TOP, fill=X, expand=YES)
+
+def stampa():
+    testo=""
+
+    if web.get():
+        testo += "Sviluppatore web"
+
+    if devops.get():
+        if mobile.get():
+            if web.get():
+                testo += ", mobile e DevOps "
+            else:
+                testo += "Sviluppatore mobile e Devops"            
+        elif web.get():
+            testo += " e DevOps "
+        else:
+            testo += "Sviluppatore DevOps "
+    elif mobile.get():
+        if web.get():
+            testo += " e mobile "
+        else:
+            testo += "Sviluppatore mobile "
+
+
+    """
+    Combinazioni
+    ### 2 valori ###
+    1. web, 
+    2. web-mobile 
+    3. mobile
+
+    ### 3 valori ###
+    1. web, 
+    2. web-mobile, 
+    3. web-devops,
+    4. web-mobile-devops,
+    5. mobile, 
+    6. mobile-devops
+    7. devops
+
+
+    """
+
+
+    """
+    # 2 Combinazioni        
+    if web.get():
+        testo += "Sviluppatore web "
+
+    if mobile.get():
+        if web.get():
+            testo += "e sviluppatore mobile"
+        else:
+            testo += "Sviluppatore mobile"
+    """
+
+    mostra.config(
+        text=testo,
+        bg="yellow"
+    )
+
+web = IntVar()
+mobile = IntVar()
+devops = IntVar()
+
+# Bottoni Check
+Label(finestra, text="A cosa ti dedichi?").grid(row=1,column=0) #pack(side=TOP, anchor=NW)
+Checkbutton(
+    finestra, 
+    text="web",
+    variable=web,
+    onvalue=1,
+    offvalue=0,
+    command=stampa
+).grid(row=2,column=0)
+
+Checkbutton(
+    finestra, 
+    text="mobile",
+    variable=mobile,
+    onvalue=1,
+    offvalue=0,
+    command=stampa
+).grid(row=3,column=0)
+
+Checkbutton(
+    finestra, 
+    text="DevOps",
+    variable=devops,
+    onvalue=1,
+    offvalue=0,
+    command=stampa
+).grid(row=4,column=0)
+
+mostra = Label(finestra)
+mostra.grid(row=5,column=0) #pack(side=TOP, anchor=NW)
+
+# Radio Buttons
+
+
+# Option Menu
 
 finestra.mainloop()
