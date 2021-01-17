d194e207d1c22d1959bf6d78ed14b266c1f9233f
Adrian Medina <medinalarry96@gmail.com>
2021-01-17 11:19:04 +0100

    Option Menu, menu a tendina del form

--- 21-tkinter/11-form-avanzato.py
+++ 21-tkinter/11-form-avanzato.py
@@ -14,76 +14,77 @@ header.config(
 )
 header.grid(row=0,column=0,columnspan=5, sticky=W) #pack(anchor=N, side=TOP, fill=X, expand=YES)
 
+"""
+Combinazioni
+### 2 valori ###
+# (a+b)^2 = a^2 + 2ab + b^2
+1. web, 
+2. web-mobile 
+3. mobile
+
+### 3 valori ###
+# (a+b+c)*2+1 = 7
+1. web, 
+2. web-mobile, 
+3. web-devops,
+4. web-mobile-devops,
+5. mobile, 
+6. mobile-devops
+7. devops
+
+### 4 valori ###
+# (a+b+c+d)*4-1 = C=n*k-1, 
+1. web, 
+2. web-mobile, 
+3. web-devops,
+4. web-ML,
+5. web-mobile-devops,
+6. web-mobile-devops-ML,
+7. web-devops-ML,
+8. web-mobile-ML,
+9. mobile, 
+10. mobile-devops,
+11. mobile-ML
+12. mobile-devops-ML,
+13. devops,
+14. devops-ML,
+15. ML
+
+1. web, 
+9. mobile, 
+13. devops,
+15. ML,
+2. web-mobile, 
+3. web-devops,
+4. web-ML,
+10. mobile-devops,
+11. mobile-ML
+14. devops-ML,
+5. web-mobile-devops,
+7. web-devops-ML,
+8. web-mobile-ML,
+12. mobile-devops-ML,
+6. web-mobile-devops-ML
+
+w
+wm
+wd
+wml
+wmlm
+wmld
+wdm
+wmmld
+m
+mml
+md
+mmld
+dml
+ml
+d
+
+"""
+
 def stampa():
-    """
-    Combinazioni
-    ### 2 valori ###
-    # (a+b)^2 = a^2 + 2ab + b^2
-    1. web, 
-    2. web-mobile 
-    3. mobile
-
-    ### 3 valori ###
-    # (a+b+c)*2+1 = 7
-    1. web, 
-    2. web-mobile, 
-    3. web-devops,
-    4. web-mobile-devops,
-    5. mobile, 
-    6. mobile-devops
-    7. devops
-
-    ### 4 valori ###
-    # (a+b+c+d)*4-1 = C=n*k-1, 
-    1. web, 
-    2. web-mobile, 
-    3. web-devops,
-    4. web-ML,
-    5. web-mobile-devops,
-    6. web-mobile-devops-ML,
-    7. web-devops-ML,
-    8. web-mobile-ML,
-    9. mobile, 
-   10. mobile-devops,
-   11. mobile-ML
-   12. mobile-devops-ML,
-   13. devops,
-   14. devops-ML,
-   15. ML
-
-    1. web, 
-    9. mobile, 
-   13. devops,
-   15. ML,
-    2. web-mobile, 
-    3. web-devops,
-    4. web-ML,
-   10. mobile-devops,
-   11. mobile-ML
-   14. devops-ML,
-    5. web-mobile-devops,
-    7. web-devops-ML,
-    8. web-mobile-ML,
-   12. mobile-devops-ML,
-    6. web-mobile-devops-ML
-
-    w
-    wm
-    wd
-    wml
-    wmlm
-    wmld
-    wdm
-    wmmld
-    m
-    mml
-    md
-    mmld
-    dml
-    ml
-    d
-
-    """
 
     testo=""
 
@@ -244,5 +245,22 @@ mostra_genere = Label(finestra)
 mostra_genere.grid(row=10,column=0)
 
 # Option Menu
+def stampaCitta():
+    lbl_citta.config(
+        text=citta.get()
+    )
+
+citta = StringVar()
+citta.set("Roma")
+
+Label(finestra, text="Città di residenza?").grid(row=7,column=1)
+
+opmCitta = OptionMenu(finestra, citta, "Roma","Milano","Bologna")
+opmCitta.grid(row=8,column=1)
+
+Button(finestra, text="Stampa", command=stampaCitta).grid(row=9,column=1)
+
+lbl_citta = Label(finestra)
+lbl_citta.grid(row=10,column=1)
 
 finestra.mainloop()
