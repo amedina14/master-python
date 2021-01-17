c4839ffdd0f242e6c1dc75e3990abc9ee2967a3d
Adrian Medina <medinalarry96@gmail.com>
2021-01-17 14:26:11 +0100

    prog tkinter: Added toolbar and exit command

--- 21-tkinter/13-progetto-tkinter-prodotti.py
+++ 21-tkinter/13-progetto-tkinter-prodotti.py
@@ -1,23 +1,37 @@
 """
 Creare un programma che abbia:
-- (fatto) Finestra
-- (fatto) Grandezza fissa
-- (fatto) No ridimensionabile
-- Un menu
+ (fatto) - Finestra
+ (fatto) - Grandezza fissa
+ (fatto) - No ridimensionabile
+ (fatto) - Un menu: Inizio, Aggiungere, Informazione, Uscire.
+ (fatto) - Opzione di uscire
 - Diverse schermate
 - Form di aggiungere prodotti
 - Salvare i dati temporalmente
 - Stampa elenco dati nella schermata home
-- Opzione di uscire
+
 """
 
 from tkinter import *
 
+# Defining window
 finestra = Tk()
 finestra.geometry("500x500")
 finestra.title("Progetto tkinter python")
 finestra.resizable(0,0)
 
+# Creates toolbar
+toolbar = Menu(finestra)
+
+# Loads toolbar: Config window to display toolbar
+finestra.config(menu=toolbar)
+
+# Defining toolbar
+toolbar.add_command(label="Inizio")
+toolbar.add_command(label="Aggiunger")
+toolbar.add_command(label="Informazione")
+toolbar.add_command(label="Uscire", command=finestra.quit)
 
 
+# Load window
 finestra.mainloop()
