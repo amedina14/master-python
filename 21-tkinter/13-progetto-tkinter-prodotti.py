0c560a265ccb21630d58fee5708d30c772810f42
Adrian Medina <medinalarry96@gmail.com>
2021-01-17 13:57:26 +0100

    progetto tkinter: requisiti, testo

---
+++ 21-tkinter/13-progetto-tkinter-prodotti.py
@@ -0,0 +1,13 @@
+"""
+Creare un programma che abbia:
+- Finestra
+- Grandezza fissa
+- No ridimensionabile
+- Un menu
+- Diverse schermate
+- Form di aggiungere prodotti
+- Salvare i dati temporalmente
+- Stampa elenco dati nella schermata home
+- Opzione di uscire
+"""
+
