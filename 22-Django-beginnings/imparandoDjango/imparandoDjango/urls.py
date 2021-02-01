f44ab4c2da6e2da2947138c3acdb49842e9e092d
Adrian Medina <medinalarry96@gmail.com>
2021-02-01 10:39:50 +0100

    url di elenco articoli

--- 22-Django-beginnings/imparandoDjango/imparandoDjango/urls.py
+++ 22-Django-beginnings/imparandoDjango/imparandoDjango/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
     path('create-article/<str:title>/<str:content>/<str:public>', views.create_article, name='create_article'),
     path('articolo/<str:id>', views.legge_articolo, name='articolo'),
     path('modifica-articolo/<str:id>', views.modifica_articolo, name='modifica_articolo'),
+    path('elenco-articoli/', views.elenco_articoli, name='elenco'),
 ]
